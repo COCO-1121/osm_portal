b0ad58d5e9069b530c8c288dfe85284e7c84d9c9
Vidhi gour <vgour6124@gmail.com>
2026-08-06 12:59:06 +0530

    Frontend fixed for examiner and admin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // <-- apne backend ka actual port yahan daalo (Django=8000, Node/Express=3000 or 5000, FastAPI=8000)
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       },
